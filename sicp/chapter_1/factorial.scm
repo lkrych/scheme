b3eba6eb998b9b53e8c6e46610220c2aae3faddf
Leland Krych <leland.krych@gmail.com>
2022-05-26 22:06:14 -0700

    use lexical scoping to define an iterative factorial function

--- sicp/chapter_1/factorial.scm
+++ sicp/chapter_1/factorial.scm
@@ -7,3 +7,16 @@
 (factorial 4)
 ;120
 (factorial 5)
+
+(define (factorial-iter x)
+  (define (fact-iter product counter)
+    (if (> counter x)
+    product
+    (fact-iter (* counter product)
+                (+ counter 1))))
+  (fact-iter 1 1))
+
+;24
+(factorial-iter 4)
+;120
+(factorial-iter 5)
