3a99f4c884ba043ca8e71bdd176c638d4a05c1df
Leland Krych <leland.krych@gmail.com>
2022-05-29 14:11:11 -0700

    add iterative solution to 1_11

--- sicp/chapter_1/1_2_2_exc.scm
+++ sicp/chapter_1/1_2_2_exc.scm
@@ -15,14 +15,23 @@
 (f 6) ;59
 (f 7) ;142
 
+; difference between values
+; 1 2 7 14 24 83
 ;write a procedure that computes f by means of an iterative process
 ;(f 5)
 ; f(4) + 2*f(3) + 3*f(2))
 ; f(4) = f (3) + (2 * f (2)) + (3* f(1)) 
-; (define (f-better n)
-;     (f-iter 1 0 n))
+(define (f-better n)
+    (define (f-iter a b c count)
+        (cond ((< n 3) n)
+        ((<= count 0) a)
+        (else (f-iter (+ a (* 2 b) (* 3 c)) a b (- count 1)))))
+    (f-iter 2 1 0 (- n 2)))
 
-; (define (f-iter a b count)
-;     (if (< count 3)
-;     3
-;     (f-iter (+ ()))
+(f-better 1) ;1
+(f-better 2) ;2
+(f-better 3) ;4
+(f-better 4) ;11
+(f-better 5) ;25
+(f-better 6) ;59
+(f-better 7) ;142
