ea5a7c964afa2e2273ba23d430a8741baebc939e
Leland Krych <leland.krych@gmail.com>
2022-06-07 21:52:28 -0700

    fix some typos in previous funcs and add integral

--- sicp/chapter_1/higher_order.scm
+++ sicp/chapter_1/higher_order.scm
@@ -14,7 +14,7 @@
 
 ;compute sum of a sequence 1/1*3 + 1/5*7 + 1/9*11...
 (define (pi-sum a b)
-  (if (a > b)
+  (if (> a b)
     0
     (+ (/ 1.0 (* a (+ a 2)))
        (pi-sum (+ a 4) b))))
@@ -46,4 +46,25 @@
 
 (sum-integers 1 10)
 (sum-integers2 1 10)
+
+(define (pi-sum2 a b)
+  (define (pi-term x)
+    (/ 1.0 (* x (+ x 2))))
+  (define (pi-next x)
+    (+ x 4))
+  (sum pi-term a pi-next b))
+
+(* 8 (pi-sum 1 1000))
+(* 8 (pi-sum2 1 1000))
+
+; define integral of function f between limits a and b
+(define (integral f a b dx)
+  (define (add-dx x)
+    (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b)
+     dx))
+
+; the exect value of the integral of the cube between 0 and 1 is 1/4
+(integral cube 0 1 0.01)
+(integral cube 0 1 0.001)
 
